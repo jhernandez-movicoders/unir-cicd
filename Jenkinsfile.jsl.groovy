a21d4db77a2265c379ff46347dc1bb2a5e21afcd
Juan José Hernández <juanjo.hernandez@movicoders.com>
2022-06-06 10:30:45 +0200

    master: mail on failure.

--- Jenkinsfile.jsl.groovy
+++ Jenkinsfile.jsl.groovy
@@ -33,5 +33,8 @@ pipeline {
             junit 'results/*_result.xml'
             cleanWs()
         }
+        failure {  
+             mail bcc: '', body: "<br>Project: ${env.JOB_NAME} <br>Build Number: ${env.BUILD_NUMBER} <br> URL de build: ${env.BUILD_URL}", cc: '', charset: 'UTF-8', from: '', mimeType: 'text/html', replyTo: '', subject: "ERROR CI: Project name -> ${env.JOB_NAME}", to: "anselm82@gmail.com";  
+        }  
     }
 }
