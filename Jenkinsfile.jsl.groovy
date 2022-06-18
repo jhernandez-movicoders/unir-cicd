5ba21c39015197b8ae023c6f1988007319852472
Juan José Hernández <juanjo.hernandez@movicoders.com>
2022-06-18 13:36:19 +0200

    master: right recipient

--- Jenkinsfile.jsl.groovy
+++ Jenkinsfile.jsl.groovy
@@ -66,7 +66,7 @@ pipeline {
         failure {  
             emailext body: 'Test Message',
                 subject: 'Test Subject',
-                to: 'test@example.com'
+                to: 'anselm82@gmail.com'
             //mail bcc: '', body: "<br>Project: ${env.JOB_NAME} <br>Build Number: ${env.BUILD_NUMBER} <br> URL de build: ${env.BUILD_URL}", cc: '', charset: 'UTF-8', from: '', mimeType: 'text/html', replyTo: '', subject: "ERROR CI: Project name -> ${env.JOB_NAME}", to: "anselm82@gmail.com";  
         }  
     }
