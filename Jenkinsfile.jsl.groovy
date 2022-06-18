c8fcb256657332fd3247abcb0ca829d9ff03794b
Juan José Hernández <juanjo.hernandez@movicoders.com>
2022-06-18 15:55:12 +0200

    masteer: more mail tests.

--- Jenkinsfile.jsl.groovy
+++ Jenkinsfile.jsl.groovy
@@ -66,6 +66,7 @@ pipeline {
         failure {  
             emailext body: 'Test Message',
                 subject: 'Test Subject',
+                from: 'juanjose.hernandez886@comunidadunir.net',
                 to: 'anselm82@gmail.com'
             //mail bcc: '', body: "<br>Project: ${env.JOB_NAME} <br>Build Number: ${env.BUILD_NUMBER} <br> URL de build: ${env.BUILD_URL}", cc: '', charset: 'UTF-8', from: '', mimeType: 'text/html', replyTo: '', subject: "ERROR CI: Project name -> ${env.JOB_NAME}", to: "anselm82@gmail.com";  
         }  
