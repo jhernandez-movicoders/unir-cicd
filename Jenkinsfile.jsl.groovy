9d2ce08e83d429a55a74f3cf03d8dede97b96df9
Juan José Hernández <juanjo.hernandez@movicoders.com>
2022-06-18 12:06:27 +0200

    master: adding new stages

--- Jenkinsfile.jsl.groovy
+++ Jenkinsfile.jsl.groovy
@@ -20,9 +20,15 @@ pipeline {
                 archiveArtifacts artifacts: 'results/*.xml'
             }
         }
-        stage('Unit api') {
+        stage('Api tests') {
             steps {
                 sh 'make test-api'
+                archiveArtifacts artifacts: 'results/*.xml'
+            }
+        }
+        stage('E2E tests') {
+            steps {
+                sh 'make test-e2e'
                 archiveArtifacts artifacts: 'results/*.xml'
             }
         }
